6589cda7b468a26b3aca093beab72d7b96731e09
Karthikeya Reddy <karthikeyareddy1908@gmail.com>
2026-03-26 10:26:01 +0530

    Change database name from 'exam_seating' to 'railway'

--- db.js
+++ db.js
@@ -12,7 +12,7 @@ const pool = mysql.createPool({
   host:             process.env.DB_HOST || "localhost",
   user:             process.env.DB_USER || "root",
   password:         process.env.DB_PASS || "FotSwCvGOzVoncQaCHLHJmXyTSXmZDJi",
-  database:         process.env.DB_NAME || "exam_seating",
+  database:         process.env.DB_NAME || "railway",
   port:             process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit:  10,
